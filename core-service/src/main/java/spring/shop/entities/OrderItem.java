4c32520e92cf4b69b4c04096a6c104f025c3b4b3
basilomp <k0m4w1@gmail.com>
2023-03-09 16:22:15 +0300

    Transitioning from Flyway to Liquibase

--- core-service/src/main/java/spring/shop/entities/OrderItem.java
+++ core-service/src/main/java/spring/shop/entities/OrderItem.java
@@ -10,7 +10,7 @@ import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "order_items")
+@Table(name = "orders_items")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
--- core-service/src/main/java/spring/shop/entities/Product.java
+++ core-service/src/main/java/spring/shop/entities/Product.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "product_table")
+@Table(name = "products")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
